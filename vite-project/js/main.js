7e48b3637856e72804b0f60439a0bf8590264ad3
Michaeli815 <113201056+Michaeli815@users.noreply.github.com>
2023-02-01 02:28:33 -0500

    gfj

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,2 +1,51 @@
 import "../styles/style.css";
-import { menu } from "./menu";
+
+const URL = "https://api.quotable.io/random";
+
+const DOMSelectors = {
+  all: document.querySelector("#all"),
+  author: document.querySelector("#author"),
+  length1: document.querySelector("#length1"),
+  quote: document.querySelector("#quote"),
+};
+
+function displayall() {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".cards").textContent = data.content;
+    })
+    .catch((error) => console.log(error));
+}
+
+function displayauthor() {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".cards").textContent = data.author;
+    })
+    .catch((error) => console.log(error));
+}
+
+function displaylength() {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".cards").textContent = data.length;
+    })
+    .catch((error) => console.log(error));
+}
+
+DOMSelectors.all.addEventListener("click", displayall);
+DOMSelectors.author.addEventListener("click", displayauthor);
+DOMSelectors.length1.addEventListener("click", displaylength);
+
+function getData(URL) {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".cards").textContent = data.content;
+    })
+    .catch((error) => console.log(error));
+}
+getData(URL);
