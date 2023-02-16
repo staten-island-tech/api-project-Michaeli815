83d4cb36dc1a3a23210f5a209896369305aa2566
michael Ivanushkin <Michaeli106@nycstudents.net>
2023-02-16 13:15:29 -0500

    xc

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -12,9 +12,6 @@ const DOMSelectors = {
   cardcontainer: document.querySelector(".card-container"),
 };
 
-function erase() {
-  DOMSelectors.cardcontainer.innerHTML = "";
-}
 
 // function ItemDisplay() {
  
@@ -43,6 +40,8 @@ function erase() {
 //   }
 // }
 
+
+
 function Displaying() {
      fetch(URL)
        .then((response) => response.json())
@@ -68,82 +67,86 @@ Displaying();
 
 
 function displayall() {
-  menu
-    .filter((menu) => menu.All === true)
-    .forEach((menu) => {
-      ItemDisplay(menu);
-    });
-}
 
-function displayHearts() {
-  menu
-    .filter((menu) => menu.suit === "HEARTS")
-    .forEach((menu) => {
-      ItemDisplay(menu);
-    });
-}
-function displaySummer() {
-  menu
-    .filter((menu) => menu.Summer === true)
-    .forEach((menu) => {
-      ItemDisplay(menu);
-    });
-}
-function displayWinter() {
-  menu
-    .filter((menu) => menu.winter === true)
-    .forEach((menu) => {
-      ItemDisplay(menu);
-    });
-}
-function displayKids() {
-  menu
-    .filter((menu) => menu.Kids === true)
-    .forEach((menu) => {
-      ItemDisplay(menu);
-    });
-}
-function displayAdults() {
-  menu
-    .filter((menu) => menu.Adult === true)
-    .forEach((menu) => {
-      ItemDisplay(menu);
-    });
-}
+    data.cards.forEach((element)=> document.querySelector(".card-container").filter (() => element.suit ==="HEARTS")
+    .forEach((element) => {
+      Displaying(element);
+    })
+)}
 
-function Designerp() {
-  erase();
-  displayDesigner();
+// function displayHearts() {
+//   menu
+//     .filter((menu) => menu.suit === "HEARTS")
+//     .forEach((menu) => {
+//       ItemDisplay(menu);
+//     });
+// }
+// function displaySummer() {
+//   menu
+//     .filter((menu) => menu.Summer === true)
+//     .forEach((menu) => {
+//       ItemDisplay(menu);
+//     });
+// }
+// function displayWinter() {
+//   menu
+//     .filter((menu) => menu.winter === true)
+//     .forEach((menu) => {
+//       ItemDisplay(menu);
+//     });
+// }
+// function displayKids() {
+//   menu
+//     .filter((menu) => menu.Kids === true)
+//     .forEach((menu) => {
+//       ItemDisplay(menu);
+//     });
+// }
+// function displayAdults() {
+//   menu
+//     .filter((menu) => menu.Adult === true)
+//     .forEach((menu) => {
+//       ItemDisplay(menu);
+//     });
+// }
+
+function erase() {
+  DOMSelectors.cardcontainer.innerHTML = "";
 }
+// 
+// function Designerp() {
+//   erase();
+//   displayDesigner();
+// }
 
 function Allp() {
   erase();
   displayall();
 }
 
-function Summerp() {
-  erase();
-  displaySummer();
-}
-function Winterp() {
-  erase();
-  displayWinter();
-}
-function Kidsp() {
-  erase();
-  displayKids();
-}
-function Adultsp() {
-  erase();
-  displayAdults();
-}
+// function Summerp() {
+//   erase();
+//   displaySummer();
+// }
+// function Winterp() {
+//   erase();
+//   displayWinter();
+// }
+// function Kidsp() {
+//   erase();
+//   displayKids();
+// }
+// function Adultsp() {
+//   erase();
+//   displayAdults();
+// }
 
-DOMSelectors.Designerb.addEventListener("click", Designerp);
+// DOMSelectors.Designerb.addEventListener("click", Designerp);
 DOMSelectors.allb.addEventListener("click", Allp);
-DOMSelectors.summerb.addEventListener("click", Summerp);
-DOMSelectors.winterb.addEventListener("click", Winterp);
-DOMSelectors.kidsb.addEventListener("click", Kidsp);
-DOMSelectors.adultsb.addEventListener("click", Adultsp);
+// DOMSelectors.summerb.addEventListener("click", Summerp);
+// DOMSelectors.winterb.addEventListener("click", Winterp);
+// DOMSelectors.kidsb.addEventListener("click", Kidsp);
+// DOMSelectors.adultsb.addEventListener("click", Adultsp);
 // const DOMSelectors = {
 //   all: document.querySelector("#all"),
 //   author: document.querySelector("#author"),
